5e3f57c62d4dde30ed747c4e65a3df9ea1ded4f4
tusharbhanarkar <113408232+tusharbhanarkar@users.noreply.github.com>
2022-09-13 11:23:56 +0530

    Add files via upload
    Initial version uploaded

---
+++ reverseString.java
@@ -0,0 +1,12 @@
+class Reverse{
+    public static void main(String[] args){
+        String str = "I am good";
+        String rev = reverse(str);       System.out.println(rev);
+  }
+    public static String reverse(String s) {  
+    int x = s.indexOf(" ");  
+    if(x == -1)
+      return s;  
+    return reverse(s.substring(x+1)) +" "+ s.substring(0, x);
+}
+}
